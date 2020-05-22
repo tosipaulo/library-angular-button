95e07ac6dfb683063798efbe84a9bc31de29a3cd
Paulo Tosi <tosi.paulo@gmail.com>
2020-05-21 21:51:30 -0300

    Creating html element and css library button

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lib-button',
   template: `
-    <p>
-      button works!:::
-    </p>
+    <span><ng-content></ng-content></span>
+    <span *ngIf="buttonIcon">i</span>
   `,
-  styles: []
+  styleUrls: ['./button.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ButtonComponent implements OnInit {
 
-  constructor() { }
+  @Input() buttonIcon: string;
+  @Input() buttonType: string;
+
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.elementRef.nativeElement.classList.add('lib-button-base');
+
+    if (this.buttonType) {
+      this.elementRef.nativeElement.classList.add(`lib-button--${this.buttonType}`);
+    }
+    
   }
 
 }
