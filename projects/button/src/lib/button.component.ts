ab893bdbb64c89268ca23c61b4c09309d905c931
Paulo Tosi <tosi.paulo@gmail.com>
2020-05-22 18:39:40 -0300

    Creating style icon material icon

--- projects/button/src/lib/button.component.ts
+++ projects/button/src/lib/button.component.ts
@@ -1,26 +1,46 @@
-import { Component, OnInit, Input, ElementRef, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ElementRef, 
+  ViewEncapsulation, 
+  ChangeDetectionStrategy
+} from '@angular/core';
+
 
 @Component({
-  selector: 'lib-button',
+  selector: 'button[lib-button], a[lib-button], lib-button',
   template: `
+    <span class="lib-button-icon" *ngIf="buttonIcon">
+      <i class="material-icons">{{buttonIcon}}</i>
+    </span>
     <span><ng-content></ng-content></span>
-    <span *ngIf="buttonIcon">i</span>
+    <span *ngIf="totalFilter">[{{totalFilter}}]</span>
   `,
-  styleUrls: ['./button.component.css'],
+  styleUrls: ['./button.component.scss'],
   encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
 
   @Input() buttonIcon: string;
   @Input() buttonType: string;
+  @Input() buttonTotalFilter: any;
+
+  totalFilter: number;
 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+    
     this.elementRef.nativeElement.classList.add('lib-button-base');
 
     if (this.buttonType) {
       this.elementRef.nativeElement.classList.add(`lib-button--${this.buttonType}`);
+    }
+
+    if (parseInt(this.buttonTotalFilter)) { 
+      this.totalFilter = parseInt(this.buttonTotalFilter);
     }
     
   }
--- projects/button/src/lib/button.module.ts
+++ projects/button/src/lib/button.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ButtonComponent } from './button.component';
-
-
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ButtonComponent],
   imports: [
+    CommonModule
   ],
   exports: [ButtonComponent]
 })
